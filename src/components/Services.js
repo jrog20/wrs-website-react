2c0d5e2f8d84243d06ac56e167379e8ce0467506
jrog20 <jaimerogstad@gmail.com>
2021-08-18 10:15:05 -0400

    Add basic info to services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,6 +5,29 @@ function Services() {
   return (
     <>
       <div className='background-image' style={{ backgroundImage: `url(${studentjumping})` }} alt="Horse">
+      {/* Need to add images, styling, and more info/text to this page */}
+      <h1>Services Offered</h1>
+        <h2>Boarding</h2>
+          <h3>Includes daily turnout and stall. Contact Roger for more details and rates.</h3>
+        <h2>Lessons</h2>
+          <h3>Offering lessons in Riding, Jumping and Driving. All levels are welcomed and encouraged.</h3>
+            Roger Poitras' method of instruction is a combination of several disciplines:
+            <ul>
+              <li>The Alexander Technique</li>
+              <li>Soma Neuromuscular Integration</li>
+              <li>The Feldenkrais Method</li>
+              <li>Centered Riding</li> 
+            </ul>
+            These disciplines have been brought together into the following elements: 
+            <ul>
+              <li>Hands-on body work to aid the rider in awareness of tensions in the body and in releasing those tensions.</li>
+              <li>Body alignment for proper balance.</li>
+              <li>Flow of energy in the development of form and movement.</li>
+              <li>Visualization for freedom of mind and body.</li> 
+            </ul>
+            The resulting effect is the body understanding, energizing and communicating with great clarity.     
+        <h2>Training</h2>
+          <h3>Roger provides training of horses in Riding, Jumping and Driving.</h3>
       </div>
     </>
   );
