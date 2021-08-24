4258059c6605060226ee184dc00680e653da6d07
jrog20 <jaimerogstad@gmail.com>
2021-08-24 11:42:23 -0400

    Comment out line causing mapping error

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,13 +3,15 @@ import Post from './Post';
 
 const Posts = ({ posts }) => (
   <div>
-    {/* <p>This is the Posts component</p> */}
+    <p>This is the Posts component</p>
     {/* This is how to render the image entered on the form */}
     {/* <p><img src={this.props.images[0].url}/></p> */}
     {/* <img src={props.post.images.url}> */}
 
     {/* MAYBE? {img_url={post.images.url} or add [0]? */}
-    { posts.map(post => <Post title={post.title} />)}
+
+
+    {/* { posts.map(post => <Post title={post.title} />)} */}
   </div>
 );
 
