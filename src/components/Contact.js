4db11c5ecd37b94115111ceb3e46db47f85c6c19
jrog20 <jaimerogstad@gmail.com>
2021-09-01 18:09:20 -0400

    Add css to about page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,12 +7,20 @@ function Contact() {
       <div className='background-image resize-image' style={{ backgroundImage: `url(${youngstudentshowing})` }} alt="Horse">
       </div>
       <h1>Contact Us</h1>
-      <h2>Address</h2>
-      <h3>102 Hill Rd, Newfane, VT 05345</h3>
-      <h2>Phone</h2>
-      <h3>(802) 365-7668</h3>
-      <h2>Email</h2>
-      <h3>westriverstables@comcast.net</h3>
+      <div className='flex-container'>
+        <div>
+          <h2>Address</h2>
+          <h3>102 Hill Rd, Newfane, VT 05345</h3>
+        </div>
+        <div>
+          <h2>Phone</h2>
+          <h3>(802) 365-7668</h3>
+        </div>
+        <div>
+          <h2>Email</h2>
+          <h3>westriverstables@comcast.net</h3>
+        </div>
+      </div>
     </>
   );
 };
