86f992f7a4b6c3e0c078d3230d83d6b9c981c576
jrog20 <jaimerogstad@gmail.com>
2021-11-08 19:17:22 -0500

    Update text

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,22 @@ function About() {
     <>
       <div className='background-image about-image'>
       </div>
-      <div>
+      <div className='text-container'>
         <h1>About Us:</h1>
-        <p>Nestled in the West River Valley, West River Stables is a full-service equestrian facility, owned and managed by Roger Poitras and Howard Osgood. English riding lessons are available for every level from beginners to advanced riders</p>
-        <h2>Our Mission:</h2>
+        <p>Nestled in the West River Valley, West River Stables is a full-service 
+        equestrian facility, owned and operated by Roger Poitras and Howard Osgood.</p>
+          
+        <p>Established in 19XX our XXX acre farm provides an outdoor riding ring, dressage course, 
+        direct access to many miles of trails, as well as a 3/4 mile track around a hay field, 
+        perfect for a walk or trot with friends or a cool down after a lesson.</p>
+
+        <p>English riding lessons are available for every level from beginners to 
+        advanced riders.</p>
+          
+        <p>Contact us today to schedule a lesson, schedule a tour of the facility, 
+        or get more information on boarding with us.</p>
+        
+        {/* <h2>Our Mission:</h2>
         <p>To land on Mars or discover a new planet.</p>
       </div>
       <div>
@@ -24,7 +36,7 @@ function About() {
         <h2>Bios:</h2>
         <p>Howard was born and raised on the farm, raised by the Dinosaurs. They taught him how to be one of the best hayers around.</p>
         <p>Roger was born in a snowstorm in Canada. As a baby he sledded down from Canada to Westminster, VT. Then in the Spring he rode his horse to Brookline, VT.</p>
-        {/* <Bios /> */}
+        <Bios /> */}
       </div>
     </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -83,7 +83,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>LOG IN</Button>}
+          {/* {button && <Button buttonStyle='btn--outline'>LOG IN</Button>} */}
         </div>
       </nav>
     </>
